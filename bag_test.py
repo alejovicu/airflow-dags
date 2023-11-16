768ff270228cfa5e420c0571af19abd79de5fb5a
Alejandro Rosero <alejandro.rosero@zebware.com>
2023-11-16 16:54:08 +0100

    test git-sync integration with airflow.

---
+++ bag_test.py
@@ -0,0 +1,10 @@
+import datetime
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+test_dag_alejandro = DAG(
+     dag_id="test_dag_alejandro",
+     start_date=datetime.datetime(2023, 11, 16),
+     schedule="@daily",
+)
+EmptyOperator(task_id="task", dag=test_dag_alejandro)
